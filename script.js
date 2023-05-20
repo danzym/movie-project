01cf9f45be0a43d588adb08c95934dc98a1d58b1
ahmet <adsayahmet94@hotmail.com>
2023-05-20 21:45:43 +0300

    added movierenderdetails part

--- script.js
+++ script.js
@@ -1,73 +1,75 @@
 'use strict';
-
+const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w185";
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
-const PROFILE_BASE_URL = 'http://image.tmdb.org/t/p/w185';
 const BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780';
 const CONTAINER = document.querySelector('.container');
 
-// Don't touch this function please
 const autorun = async () => {
-	const movies = await fetchMovies();
-	renderMovies(movies.results);
+  const movies = await fetchMovies();
+  renderMovies(movies.results);
 };
 
-// Don't touch this function please
 const constructUrl = (path) => {
-	return `${TMDB_BASE_URL}/${path}?api_key=${atob(
-		'NGViNmRiNGQ1MjBmOGNkNWYzZWY4Y2JjZjU5ZTZhMDI='
-	)}`;
+  return `${TMDB_BASE_URL}/${path}?api_key=${atob(
+    'NGViNmRiNGQ1MjBmOGNkNWYzZWY4Y2JjZjU5ZTZhMDI='
+  )}`;
 };
 
-// You may need to add to this function, definitely don't delete it.
 const movieDetails = async (movie) => {
 	const movieRes = await fetchMovie(movie.id);
 	renderMovie(movieRes);
 };
 
-// This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
 const fetchMovies = async () => {
-	const url = constructUrl(`movie/now_playing`);
-	const res = await fetch(url);
-	return res.json();
+  const url = constructUrl('movie/now_playing');
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 };
 
-// Don't touch this function please. This function is to fetch one movie.
 const fetchMovie = async (movieId) => {
 	const url = constructUrl(`movie/${movieId}`);
 	const res = await fetch(url);
 	return res.json();
 };
-
-// You'll need to play with this function in order to add features and enhance the style.
 const renderMovies = (movies) => {
-	movies.map((movie) => {
-		const movieDiv = document.createElement('div');
-		movieDiv.innerHTML = `
-        <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${
-			movie.title
-		} poster">
-        <h3>${movie.title}</h3>`;
-		movieDiv.addEventListener('click', () => {
-			movieDetails(movie);
-		});
-		CONTAINER.appendChild(movieDiv);
-	});
+  const sliderContainer = document.querySelector('.slider-container');
+  const slider = document.createElement('div');
+  slider.className = 'slider';
+
+  movies.forEach((movie) => {
+    const slide = document.createElement('div');
+    slide.className = 'slide';
+
+    const img = document.createElement('img');
+    img.src = BACKDROP_BASE_URL + movie.backdrop_path;
+    img.alt = movie.title;
+
+    slide.appendChild(img);
+    slider.appendChild(slide);
+
+    img.addEventListener('click', () => {
+      movieDetails(movie)
+    })
+  });
+
+  sliderContainer.appendChild(slider);
 };
 
-// You'll need to play with this function in order to add features and enhance the style.
+
 const renderMovie = (movie) => {
-	CONTAINER.innerHTML = `
+  CONTAINER.innerHTML = `
     <div class="row">
         <div class="col-md-4">
              <img id="movie-backdrop" src=${
-								BACKDROP_BASE_URL + movie.backdrop_path
-							}>
+               BACKDROP_BASE_URL + movie.backdrop_path
+             }>
         </div>
         <div class="col-md-8">
             <h2 id="movie-title">${movie.title}</h2>
             <p id="movie-release-date"><b>Release Date:</b> ${
-							movie.release_date
-						}</p>
+              movie.release_date
+            }</p>
             <p id="movie-runtime"><b>Runtime:</b> ${movie.runtime} Minutes</p>
             <h3>Overview:</h3>
             <p id="movie-overview">${movie.overview}</p>
@@ -76,6 +78,24 @@ const renderMovie = (movie) => {
             <h3>Actors:</h3>
             <ul id="actors" class="list-unstyled"></ul>
     </div>`;
+};
+
+const prevSlide = () => {
+  const slider = document.querySelector('.slider');
+  const slideWidth = slider.scrollWidth / 3;
+  slider.scrollBy({
+    left: -slideWidth,
+    behavior: 'smooth',
+  });
+};
+
+const nextSlide = () => {
+  const slider = document.querySelector('.slider');
+  const slideWidth = slider.scrollWidth / 3;
+  slider.scrollBy({
+    left: slideWidth,
+    behavior: 'smooth',
+  });
 };
 
 document.addEventListener('DOMContentLoaded', autorun);
